0bb39135f3c339689d426a26a095dbbdbfd6b6f4
oracleofnj <jared.samet@aya.yale.edu>
2016-02-19 11:25:45 -0500

    Improve mobile UI

--- HTML/js/main.js
+++ HTML/js/main.js
@@ -1,6 +1,10 @@
 var theApp = (function() {
   var outerSVG = d3.select("#treemap");
+  var isNarrow = outerSVG.node().getBoundingClientRect().width < 600;
   outerSVG.style("height", outerSVG.style("width")); // make square
+  if (isNarrow) {
+    d3.select("#tooltip-text").classed("hidden", false);
+  }
   var repoMap = {fullDict: {}, leafList: [], leafDict: {}};
 
   function countChildren(node) {
@@ -50,125 +54,6 @@ var theApp = (function() {
     }
   }
 
-  function createTreeMap(root, level) {
-
-    function showToolTip(d) {
-      tooltip.selectAll("tspan").remove();
-      tooltip.selectAll("tspan")
-        .data(tooltipText(d))
-        .enter().append("tspan")
-        .text(function(tooltipLine) {return tooltipLine;})
-        .attr("x","0") // this will be overwritten below once we know how wide the box is
-        .attr("dy", "1.2em");
-
-      tooltipG
-        .attr("class", "tooltipgroup") //unhide
-        .attr("transform", "translate(" + d.x + "," + (d.y - d.r) + ")");
-
-      var textRect = tooltip[0][0].getBBox();
-
-      tooltip
-        .attr("y", -textRect.height-20)
-        .selectAll("tspan")
-        .attr("x", -textRect.width / 2);
-
-      tooltipBackground
-        .attr("width", textRect.width+10)
-        .attr("height", textRect.height+10)
-        .attr("x", textRect.x - textRect.width/2 - 5)
-        .attr("y", -textRect.height-20);
-    }
-
-    function hideToolTip(d) {
-      tooltipG.attr("class", "tooltipgroup hidden")
-    }
-
-    function addInnerMap(d) {
-      if (d.children) {
-        hideToolTip();
-        createTreeMap(repoMap.fullDict[d.breadcrumbs], d.depth+level);
-      } else {
-        if (repoMap.fullDict[d.breadcrumbs].repoID === parseInt($("#selected-repo").val(),10)) {
-          $("#selected-repo").val(null);
-        } else {
-          $("#selected-repo").val(repoMap.fullDict[d.breadcrumbs].repoID);
-        }
-      }
-      $("#selected-repo").trigger("change");
-    }
-
-    var containerWidth = parseFloat(outerSVG.style("width"));
-    var margin=(1-Math.pow(0.95,level)) * containerWidth, diameter=containerWidth-2*margin;
-
-    var color = d3.scale.linear()
-        .domain([-1, 5])
-        .range(["hsl(152,80%,80%)", "hsl(228,30%,40%)"])
-        .interpolate(d3.interpolateHcl);
-
-    var pack = d3.layout.pack()
-        .padding(2)
-        .size([diameter, diameter])
-        .value(function() {return 1;});
-
-    var innerSVG = outerSVG.append("g")
-      .attr("transform","translate(" + margin + "," + margin + ")");
-
-    // d3 is going to mutate the object - make a deep copy before passing it in
-    var nodes = pack.nodes(JSON.parse(JSON.stringify(root)));
-
-    var circles = innerSVG.append("g")
-      .attr("class", "circle-container")
-      .selectAll("circle")
-        .data(nodes)
-      .enter().append("circle")
-        .attr("class", function(d) { return d.sanitizedName + " " + (d.parent ? d.children ? "node" : "node node--leaf" : "node node--root"); })
-        .style("fill", function(d) { return d.children ? color(level + d.depth - 1) : null; })
-        .attr("r", function(d) {return d.r;})
-        .attr("cx", function(d) {return d.x;})
-        .attr("cy", function(d) {return d.y;})
-        .on("mouseover", showToolTip)
-        .on("mouseout", hideToolTip)
-        .on("click", addInnerMap);
-
-    var tooltipG = innerSVG.append("g")
-      .attr("class", "tooltipgroup hidden");
-    var tooltipBackground = tooltipG.append("rect")
-      .attr("class", "tooltipBackground")
-      .attr("rx", 5)
-      .attr("ry", 5);
-    var tooltipTriangle = tooltipG.append("polygon")
-      .attr("points", "-10,-10 10,-10 0,-3")
-      .attr("class", "tooltipTriangle");
-    var tooltip = tooltipG.append("text");
-
-    if (level > 1) {
-      var closeButtonG = innerSVG.append("g")
-        .attr("class", "closebuttongroup")
-        .attr("transform", "translate(" + (diameter - diameter*0.1) + "," + (diameter*0.1) + ")");
-
-      closeButtonG.append("line")
-        .attr("class", "closebuttonx")
-        .attr("x1", diameter * 0.03 * (1 - Math.sqrt(2)/2))
-        .attr("y1", diameter * 0.03 * (1 - Math.sqrt(2)/2))
-        .attr("x2", diameter * 0.03 * (1 + Math.sqrt(2)/2))
-        .attr("y2", diameter * 0.03 * (1 + Math.sqrt(2)/2));
-
-      closeButtonG.append("line")
-        .attr("class", "closebuttonx")
-        .attr("x1", diameter * 0.03 * (1 - Math.sqrt(2)/2))
-        .attr("y1", diameter * 0.03 * (1 + Math.sqrt(2)/2))
-        .attr("x2", diameter * 0.03 * (1 + Math.sqrt(2)/2))
-        .attr("y2", diameter * 0.03 * (1 - Math.sqrt(2)/2));
-
-      closeButtonG.append("circle")
-        .attr("class", "closebuttoncircle")
-        .attr("cx", diameter * 0.03)
-        .attr("cy", diameter * 0.03)
-        .attr("r", diameter * 0.03)
-        .on("click", function() { innerSVG.remove(); });
-    }
-  }
-
   function selectRepo(selectedRepo) {
     d3.selectAll(".node")
       .classed("selected", false);
@@ -180,6 +65,224 @@ var theApp = (function() {
       }
     }
   }
+
+  function createTreeMap(root, level) {
+    function showToolTip(node, svgCircle) {
+      var repoInfo = tooltipText(node);
+      circles.classed("outlined", false);
+      highlightedSVGCircle = svgCircle.classed("outlined", true);
+
+      if (isNarrow) {
+        tooltip.html(repoInfo.join(", "));
+      } else {
+        tooltip.selectAll("tspan").remove();
+        tooltip.selectAll("tspan")
+          .data(repoInfo)
+          .enter().append("tspan")
+          .text(function(tooltipLine) {return tooltipLine;})
+          .attr("x","0") // this will be overwritten below once we know how wide the box is
+          .attr("dy", "1.2em");
+
+        tooltipG
+          .classed("hidden", false) //unhide
+          .attr("transform", "translate(" + node.x + "," + (node.y - node.r) + ")");
+
+        var textRect = tooltip[0][0].getBBox();
+
+        tooltip
+          .attr("y", -textRect.height-20)
+          .selectAll("tspan")
+          .attr("x", -textRect.width / 2);
+
+        tooltipBackground
+          .attr("width", textRect.width+10)
+          .attr("height", textRect.height+10)
+          .attr("x", textRect.x - textRect.width/2 - 5)
+          .attr("y", -textRect.height-20);
+      }
+    }
+
+    function hideToolTip() {
+      circles.classed("outlined", false);
+      highlightedSVGCircle = null;
+      if (isNarrow) {
+        tooltip.html("");
+      } else {
+        tooltipG.classed("hidden", true);
+      }
+    }
+
+    function addInnerMap(node) {
+      if (node.children) {
+        hideToolTip();
+        createTreeMap(repoMap.fullDict[node.breadcrumbs], node.depth+level);
+      } else {
+        if (repoMap.fullDict[node.breadcrumbs].repoID === parseInt($("#selected-repo").val(),10)) {
+          $("#selected-repo").val(null);
+        } else {
+          $("#selected-repo").val(repoMap.fullDict[node.breadcrumbs].repoID);
+        }
+      }
+      $("#selected-repo").trigger("change");
+    }
+
+    var containerWidth = parseFloat(outerSVG.style("width"));
+    var margin = isNarrow ? 5 : containerWidth * (1-Math.pow(0.95,level));
+    var diameter=containerWidth-2*margin;
+    var tooltip;
+
+    var pack = d3.layout.pack()
+        .padding(3)
+        .size([diameter, diameter])
+        .value(function() {return 1;});
+
+    var innerSVG = outerSVG.append("g")
+      .attr("transform","translate(" + margin + "," + margin + ")");
+
+    innerSVG.append("rect") // block mouse events
+      .attr("x", 0).attr("y", 0).attr("width", diameter).attr("height", diameter).attr("fill", "none").attr("pointer-events", "all");
+
+    // d3 is going to mutate the object - make a deep copy before passing it in
+    var nodes = pack.nodes(JSON.parse(JSON.stringify(root)));
+
+    var circles = innerSVG.append("g")
+      .attr("class", "circle-container")
+      .selectAll("circle")
+        .data(nodes)
+      .enter().append("circle")
+        .attr("class", function(d) {
+          return d.sanitizedName + " " +
+                (d.parent ? d.children ? "node" : "node node--leaf" : "node node--root") +
+                (d.children ? " level" + (level + d.depth - 1) : "");
+        })
+        .attr("r", function(d) {return d.r;})
+        .attr("cx", function(d) {return d.x;})
+        .attr("cy", function(d) {return d.y;});
+
+    if (isNarrow) {
+      circles
+        .style("pointer-events", "none")
+        .filter(function(d) { return d.depth === 1; })
+        .style("pointer-events", "all");
+    }
+
+    // Set up events.
+    // Mouseover highlights a node.
+    // Click highlights a node and immediately expands it.
+    // Mouseout un-highlights a node.
+    //
+    // For an unhighlighted node, touchstart highlights it and
+    // sets a timeout to expand it if not cancelled by touchend.
+    // If a second touchstart comes in while
+    // the first touchstart hasn't ended, the event is tossed.
+    // For a highlighted node, touchstart cancels any existing
+    // timeout and immediately expands it.
+    // touchend and touchcancel cancel any existing timeouts.
+
+    var highlightedSVGCircle=null, timeoutFnID=null, activeTouches=0;
+    circles
+      .on("mouseover", function(d) {
+        showToolTip(d, d3.select(this));
+      })
+      .on("mouseout", function(d) {
+        hideToolTip();
+      })
+      .on("click", function(d) {
+        addInnerMap(d);
+      })
+      .on("touchstart", function(d) {
+        console.log("touch started", d, d3.event, this);
+        d3.event.preventDefault();
+        d3.event.stopPropagation();
+
+        activeTouches++;
+        if (activeTouches > 1) {
+          return;
+        }
+
+        if (highlightedSVGCircle && (highlightedSVGCircle[0][0] === d3.select(this)[0][0])) {
+          // if they re-touched the already highlighted circle,
+          // cancel existing timeout and immediately expand it
+          if (timeoutFnID !== null) {
+            clearTimeout(timeoutFnID);
+            timeoutFnID = null;
+          }
+          addInnerMap(d);
+        } else {
+          // if they touched a new circle, highlight it
+          showToolTip(d, d3.select(this));
+          timeoutFnID = setTimeout(function() {addInnerMap(d);}, 1000);
+        }
+      })
+      .on("touchend", function(d) {
+        console.log("touch ended", d, d3.event, this);
+        activeTouches--;
+
+        if (timeoutFnID !== null) {
+          clearTimeout(timeoutFnID);
+          timeoutFnID = null;
+        }
+      })
+      .on("touchcancel", function(d) {
+        console.log("touch cancelled", d, d3.event, this);
+        activeTouches--;
+
+        if (timeoutFnID !== null) {
+          clearTimeout(timeoutFnID);
+          timeoutFnID = null;
+        }
+      });
+
+    if (isNarrow) {
+      tooltip = d3.select("#tooltip-text");
+    } else {
+      var tooltipG = innerSVG.append("g")
+        .attr("class", "tooltipgroup hidden");
+      var tooltipBackground = tooltipG.append("rect")
+        .attr("class", "tooltipBackground")
+        .attr("rx", 5)
+        .attr("ry", 5);
+      var tooltipTriangle = tooltipG.append("polygon")
+        .attr("points", "-10,-10 10,-10 0,-3")
+        .attr("class", "tooltipTriangle");
+
+      tooltip = tooltipG.append("text");
+    }
+
+    if (level > 1) {
+      var cbRadius = diameter * (isNarrow ? 0.05 : 0.03);
+      var closeButtonG = innerSVG.append("g")
+        .attr("class", "closebuttongroup")
+        .attr("transform", "translate(" + (diameter - 3 * cbRadius) + "," + (3 * cbRadius) + ")");
+
+      closeButtonG.append("line")
+        .attr("class", "closebuttonx")
+        .attr("x1", cbRadius * (1 - Math.sqrt(2)/2))
+        .attr("y1", cbRadius * (1 - Math.sqrt(2)/2))
+        .attr("x2", cbRadius * (1 + Math.sqrt(2)/2))
+        .attr("y2", cbRadius * (1 + Math.sqrt(2)/2));
+
+      closeButtonG.append("line")
+        .attr("class", "closebuttonx")
+        .attr("x1", cbRadius * (1 - Math.sqrt(2)/2))
+        .attr("y1", cbRadius * (1 + Math.sqrt(2)/2))
+        .attr("x2", cbRadius * (1 + Math.sqrt(2)/2))
+        .attr("y2", cbRadius * (1 - Math.sqrt(2)/2));
+
+      closeButtonG.append("circle")
+        .attr("class", "closebuttoncircle")
+        .attr("cx", cbRadius)
+        .attr("cy", cbRadius)
+        .attr("r", cbRadius)
+        .on("click", function() { innerSVG.remove(); })
+        .on("touchstart", function(d) {
+          d3.event.stopPropagation();
+          d3.event.preventDefault();
+          innerSVG.remove();
+        });
+    }
+  }
+
 
   return {
     createTreeMap: createTreeMap,
@@ -312,7 +415,7 @@ $(document).ready(function () {
         theme: "classic",
         placeholder: "Type or click on the map to select a repository...",
         allowClear: true,
-        data: theApp.getAllChildren(repoTree)
+        data: theApp.getAllChildren(repoTree).sort(function(a,b) {return a.text.localeCompare(b.text);})
       });
       $("#selected-repo-placeholder").addClass("hidden");
       $sr.removeClass("hidden");
