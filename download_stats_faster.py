3a17323c87f0f2b6b97034d88e73ec41a3b60e85
oracleofnj <jared.samet@aya.yale.edu>
2016-03-05 18:25:07 -0500

    Make crawl more efficient

---
+++ download_stats_faster.py
@@ -0,0 +1,195 @@
+# Uses PyGithub by Vincent Jacques @jacquev6
+from github import *
+from collections import OrderedDict
+import datetime
+import time
+import itertools as it
+import math
+import json
+import gzip
+
+def load_repos(g, data_path):
+    try:
+        with gzip.open(data_path + "/cached_repos.json.gz", "r") as cached_repos:
+            repos = json.loads(cached_repos.read())
+            return repos #OrderedDict(sorted(repos.iteritems(), key=lambda i: -i[1]["stargazers_count"]))
+    except IOError:
+        # No cached data, pre-populate with the first 10 repos
+        return {repo.owner.login + "/" + repo.name: {
+                    "repoObj": repo,
+                    "id": repo.id,
+                    "stargazers_count": repo.stargazers_count,
+                    "crawled": False
+                } for repo in g.search_repositories("stars:>1", sort="stars", order="desc")[:10]}
+        # return OrderedDict([(repo.owner.login + "/" + repo.name,
+        #                         {"repoObj": repo,
+        #                          "id": repo.id,
+        #                          "stargazers_count": repo.stargazers_count,
+        #                          "crawled": False}) \
+        #                     for repo in g.search_repositories("stars:>1", sort="stars", order="desc")[:10]])
+    else:
+        raise
+
+def load_users(g, data_path):
+    try:
+        with gzip.open(data_path + "/cached_users.json.gz", "r") as cached_users:
+            users = json.loads(cached_users.read())
+            return users #OrderedDict(sorted(users.iteritems(), key=lambda i: -i[1]["starweight"]))
+    except IOError:
+        # No cached data, will just get crawled
+        return {}
+    else:
+        raise
+
+def save_repos(repo_dict, data_path):
+    repos_as_json = json.dumps({key: {subkey: subval for (subkey, subval) in val.items() if subkey != "repoObj"} for (key, val) in repo_dict.items()})
+    with gzip.open(data_path + "/cached_repos.json.gz", "w") as cached_repos:
+        cached_repos.write(repos_as_json)
+
+def save_users(user_dict, data_path):
+    users_as_json = json.dumps({key: {subkey: subval for (subkey, subval) in val.items() if subkey != "userObj"} for (key, val) in user_dict.items()})
+    with gzip.open(data_path + "/cached_users.json.gz", "w") as cached_users:
+        cached_users.write(users_as_json)
+
+def get_contributors(repo):
+    try:
+        return [(contributor, contributor.login, contributor.contributions, contributor.id) \
+                for contributor in repo.get_contributors()], True
+    except GithubException as e:
+        if e.data[u'message'] == u'The history or contributor list is too large to list contributors for this repository via the API.':
+            # Linux!!
+            return [(repo.owner, repo.owner.login, 100, repo.owner.id)], True
+        elif e.data[u'message'] == u'Not Found':
+            return [], False
+        elif e.data[u'message'] == u'Repository access blocked':
+            print "Github Exception: ", e.data[u'block']
+            return [], False
+        else:
+            raise
+
+def get_stars(user):
+#    try:
+        return [repo for repo in user.get_starred()], True
+#    except GithubException as e:
+#        print "Github Exceptione.data
+#        return [], False
+
+def get_next(crawlables):
+    next_crawlable = next(it.dropwhile(lambda x: x[1]["crawled"], crawlables.iteritems()))
+    return next_crawlable[0], next_crawlable[1]
+
+def get_crawl_list(crawlable, sort_by):
+    return OrderedDict(sorted([(key, val[sort_by]) for (key, val) in crawlable.iteritems() if not val["crawled"]],
+                                key = lambda i:i[1],
+                                reverse=True))
+
+def reorder(d):
+    return OrderedDict(sorted(d.iteritems(), key = lambda i:i[1], reverse=True))
+
+def process_contributors(next_repo_key, contributors, to_crawl):
+    to_crawl["repos"][next_repo_key]["contributors"] = dict()
+    total_contribs = sum([math.log1p(cTuple[2]) for cTuple in contributors])
+    to_crawl["repos"][next_repo_key]["total_log1p_contribs"] = total_contribs
+    total_starweight = to_crawl["repos"][next_repo_key]["stargazers_count"]
+    for cTuple in contributors:
+        to_crawl["repos"][next_repo_key]["contributors"][cTuple[1]] = \
+                {"contributions": cTuple[2],
+                 "log1p_contributions": math.log1p(cTuple[2]),
+                 "id": cTuple[3]}
+        if cTuple[1] in to_crawl["users"]:
+            # Add their proportional share of star count
+            to_crawl["users"][cTuple[1]]["starweight"] += \
+                 total_starweight * math.log1p(cTuple[2]) / total_contribs
+        else:
+            to_crawl["users"][cTuple[1]] = \
+                {"id": cTuple[3],
+                 "userObj": cTuple[0],
+                 "starweight": total_starweight * math.log1p(cTuple[2]) / total_contribs,
+                 "crawled": False}
+        to_crawl["ordered_users"][cTuple[1]] = to_crawl["users"][cTuple[1]]["starweight"]
+    to_crawl["ordered_users"] = reorder(to_crawl["ordered_users"])
+
+def process_stars(next_user_key, stars, to_crawl):
+    to_crawl["users"][next_user_key]["stars"] = dict()
+    for star in stars:
+        repo_fullname = star.owner.login + "/" + star.name
+        to_crawl["users"][next_user_key]["stars"][repo_fullname] = \
+                {"id": star.id}
+        if repo_fullname in to_crawl["repos"]:
+            pass # for now
+        else:
+            to_crawl["repos"][repo_fullname] = \
+                {"repoObj": star,
+                 "id": star.id,
+                 "stargazers_count": star.stargazers_count,
+                 "crawled": False}
+            to_crawl["ordered_repos"][repo_fullname] = star.stargazers_count
+    to_crawl["ordered_repos"] = reorder(to_crawl["ordered_repos"])
+
+def mark_repo_complete(to_crawl, next_repo_key, success):
+    to_crawl["repos"][next_repo_key]["crawled"] = True
+    del to_crawl["ordered_repos"][next_repo_key]
+    if not success:
+        to_crawl["repos"][next_repo_key]["failed"] = True
+
+def mark_user_complete(to_crawl, next_user_key, success):
+    to_crawl["users"][next_user_key]["crawled"] = True
+    del to_crawl["ordered_users"][next_user_key]
+    if not success:
+        to_crawl["users"][next_user_key]["failed"] = True
+
+def crawl_github(git_uname, git_pw, data_path):
+    while True:
+        g = Github(login_or_token=git_uname, password=git_pw, per_page=100)
+        to_crawl = {"repos": load_repos(g, data_path), "users": load_users(g, data_path)}
+        to_crawl["ordered_repos"] = get_crawl_list(to_crawl["repos"], "stargazers_count")
+        to_crawl["ordered_users"] = get_crawl_list(to_crawl["users"], "starweight")
+        i = 0
+        print "Logged in as %s" % g.get_user().name ## temporary hack to reset rate limiting
+        while g.rate_limiting[0] > 250:
+            try:
+                print "API calls remaining before %s: %d" % (
+                    datetime.datetime.fromtimestamp(g.rate_limiting_resettime).strftime('%Y-%m-%d %H:%M:%S'),
+                    g.rate_limiting[0])
+                next_repo_key = next(to_crawl["ordered_repos"].iteritems())[0]
+                next_repo_val = to_crawl["repos"][next_repo_key]
+                print "Processing repo: %s (%d stars)" % (next_repo_key, next_repo_val["stargazers_count"])
+                if "repoObj" not in next_repo_val: # cached from disk
+                    next_repo_val["repoObj"] = g.get_repo(next_repo_key)
+                contributors, success = get_contributors(next_repo_val["repoObj"])
+                if success:
+                    process_contributors(next_repo_key, contributors, to_crawl)
+                mark_repo_complete(to_crawl, next_repo_key, success)
+
+                next_user_key = next(to_crawl["ordered_users"].iteritems())[0]
+                next_user_val = to_crawl["users"][next_user_key]
+                print "Processing user: %s (%f starweight)" % (next_user_key, next_user_val["starweight"])
+                if "userObj" not in next_user_val: # cached from disk
+                    next_user_val["userObj"] = g.get_user(next_user_key)
+                stars, success = get_stars(next_user_val["userObj"])
+                if success:
+                    process_stars(next_user_key, stars, to_crawl)
+                mark_user_complete(to_crawl, next_user_key, success)
+
+                # save progress every 25 users/repos
+                i = i + 1
+                if i % 25 == 0:
+                    print "Processed %d times, saving..." % i
+                    save_repos(to_crawl["repos"], data_path)
+                    save_users(to_crawl["users"], data_path)
+            except Exception as e:
+                print e
+                print "Going to sleep for an hour and a half..."
+                time.sleep(90*60)
+                print "Breaking out of the inner loop and resuming from saved..."
+                break
+
+    print g.rate_limiting
+    save_repos(to_crawl["repos"], data_path)
+    save_users(to_crawl["users"], data_path)
+    return to_crawl
+
+if __name__ == "__main__":
+    username = raw_input("GitHub username: ")
+    password = raw_input("GitHub password: ")
+    crawl_github(username, password, "./downloaded_data")
---
+++ process_stats_better.py
@@ -0,0 +1,249 @@
+from __future__ import division
+import itertools as it
+from collections import OrderedDict
+import json
+import gzip
+import math
+from operator import itemgetter
+
+bots = set(["gitter-badger", "ReadmeCritic", "invalid-email-address", "bitdeli-chef",
+            "greenkeeperio-bot"])
+
+def load_repos(data_path):
+    with gzip.open(data_path + "/cached_repos.json.gz", "r") as cached_repos:
+        repos = json.loads(cached_repos.read())
+        return OrderedDict(sorted(repos.iteritems(), key=lambda i: -i[1]["stargazers_count"]))
+
+def load_users(data_path):
+    with gzip.open(data_path + "/cached_users.json.gz", "r") as cached_users:
+        users = json.loads(cached_users.read())
+        return OrderedDict(sorted(users.iteritems(), key=lambda i: -i[1]["starweight"]))
+
+def get_crawled(crawlable):
+    return {k: v for (k, v) in crawlable.iteritems() if v["crawled"] and "failed" not in v}
+
+def is_bot(user):
+    return user in bots
+
+def calc_graph(repos, users):
+    crawled_repos, crawled_users = get_crawled(repos), get_crawled(users)
+
+    links = {}
+    contrib_counts = {}
+
+    # Create links from repo to contributors
+    for (repo, repoval) in crawled_repos.iteritems():
+        if "contributors" in repoval and len(repoval["contributors"]) > 0:
+            total_log1p_contribs = repoval["total_log1p_contribs"]
+            for (contributor, contribval) in repoval["contributors"].iteritems():
+                if not is_bot(contributor):
+                    if contributor in links:
+                        links[contributor][1][repo] = contribval["log1p_contributions"] / total_log1p_contribs
+                    else:
+                        links[contributor] = ("user", {repo: contribval["log1p_contributions"] / total_log1p_contribs})
+
+                    if contributor in contrib_counts:
+                        contrib_counts[contributor] = contrib_counts[contributor] + 1
+                    else:
+                        contrib_counts[contributor] = 1
+
+    # Create links from contributors to repo
+    for (repo, repoval) in crawled_repos.iteritems():
+        if "contributors" in repoval and len(repoval["contributors"]) > 0:
+            links[repo] = ("repo", {contributor: 1.0/contrib_counts[contributor] \
+                                    for contributor in repoval["contributors"].keys()\
+                                    if not is_bot(contributor)}, {})
+
+    # Create links from starrers to repo
+    user_starcounts = {linker: len([s for s in crawled_users[linker]["stars"].keys() if s in links]) for linker in crawled_users.keys()}
+    for (user, userval) in crawled_users.iteritems():
+        if not is_bot(user):
+            if "stars" in userval:
+                starcount = len([s for s in userval["stars"].keys() if s in crawled_repos])
+                for (repo, repoval) in userval["stars"].iteritems():
+                    if repo in links:
+                        links[repo][2][user] = 1.0 / starcount
+
+    return links
+
+def calc_gitrank_graph(links, iters=25, damping=0.85, contrib_prob=0.33333):
+    num_nodes = len(links)
+    users = [key for (key, val) in links.iteritems() if val[0] == "user"]
+    repos = [key for (key, val) in links.iteritems() if val[0] == "repo"]
+    ranks = {key: 1.0/num_nodes for key in links.keys()}
+
+    for i in xrange(iters):
+        print "round {0}".format(i+1)
+        newranks = {}
+        for user in users:
+            # only get to a user from a repo
+            newranks[user] = (1.0 - damping) / num_nodes \
+                            + damping * sum([ranks[repo]*weight for (repo, weight) in links[user][1].iteritems()])
+
+        for repo in repos:
+            # two sums
+            newranks[repo] = (1.0 - damping) / num_nodes \
+                            + damping * contrib_prob * sum([ranks[user]*weight for (user, weight) in links[repo][1].iteritems()]) \
+                            + damping * (1 - contrib_prob) * sum([ranks[user]*weight for (user, weight) in links[repo][2].iteritems()])
+
+        ranks = newranks
+
+    return OrderedDict(sorted([(repo, ranks[repo]) for repo in repos], key=lambda x: -x[1])), \
+            OrderedDict(sorted([(user,
+                                (ranks[user],
+                                 OrderedDict(sorted([(repo, damping * ranks[repo] * weight) \
+                                                    for (repo, weight) in links[user][1].iteritems()], key=lambda x: -x[1])))) \
+                                for user in users], key=lambda x: -x[1][0]))
+
+def repo_to_repo_links(links, contrib_prob=0.33333):
+    repos = [key for (key, val) in links.iteritems() if val[0] == "repo"]
+    repo_to_repo = {linked_to: {linker: 0 for linker in repos} for linked_to in repos}
+    ig1 = itemgetter(1)
+    for linked_to in repos:
+        for (user, userweight) in links[linked_to][1].iteritems():
+            for (linker, linkerweight) in links[user][1].iteritems():
+                repo_to_repo[linked_to][linker] = repo_to_repo[linked_to][linker] \
+                                                + contrib_prob * linkerweight * userweight
+        for (user, userweight) in links[linked_to][2].iteritems():
+            for (linker, linkerweight) in links[user][1].iteritems():
+                repo_to_repo[linked_to][linker] = repo_to_repo[linked_to][linker] \
+                                                + (1-contrib_prob) * linkerweight * userweight
+        repo_to_repo[linked_to] = OrderedDict([x for x in sorted(repo_to_repo[linked_to].iteritems(), key=ig1, reverse=True) if x[1] >= 0.001])
+
+    linkedrepos = sorted([(r1, r2, repo_to_repo[r1][r2], repo_to_repo[r2][r1]) \
+                           for r1 in repos for r2 in repos \
+                           if r1 in repo_to_repo[r2] and r2 in repo_to_repo[r1] and r1 < r2],
+                          key = lambda x: -x[2]-x[3])
+
+    return repo_to_repo, linkedrepos
+
+def calc_similarities(r2r, repos, initial_pref=0, num_iters=10, damping=0.95):
+    ig1 = itemgetter(1)
+    selfsims = sorted([(repo, r2r[repo][repo]) for repo in r2r.keys()], key=ig1, reverse=True)
+    selfsims_dict = {repo: selfsim for (repo, selfsim) in selfsims}
+    starred_repos = sorted([(repo, repos[repo]["stargazers_count"], selfsims_dict[repo]) for repo in selfsims_dict.keys()], key=ig1, reverse=True)
+    ordered_repos = [repo for (repo, sg, ss) in starred_repos]
+    starcounts_dict = {repo: sg for (repo, sg, ss) in starred_repos}
+
+    sim = {}
+    for (exemplar, points) in r2r.iteritems():
+        for (point, weight) in points.iteritems():
+            if point not in sim:
+                sim[point] = {}
+            sim[point][exemplar] = weight if point != exemplar else initial_pref
+            ### math.log(starcounts_dict[exemplar]/100) * weight if point != exemplar else initial_pref
+
+    avail = {exemplar: {point: 0 \
+            for point in r2r[exemplar].keys()} for exemplar in ordered_repos}
+
+    oldresp, oldavail, damp = None, None, 1
+    for i in xrange(num_iters):
+        # todo: fix damping
+        resp = {}
+        for point in sim.keys():
+            avail_plus_sim = [(cand, avail[cand][point] + sim[point][cand]) \
+                                for cand in sim[point].keys()]
+            best, second_best = (None, 0), (None, 0)
+            for (cand, a_plus_s) in avail_plus_sim:
+                if a_plus_s > best[1]:
+                    second_best = best
+                    best = (cand, a_plus_s)
+                elif a_plus_s > second_best[1]:
+                    second_best = (cand, a_plus_s)
+                else:
+                    pass
+
+            resp[point] = {exemplar: \
+                            (oldresp[point][exemplar]*(1-damp) if oldresp != None else 0) + \
+                            (damp if oldresp != None else 1) * \
+                            (sim[point][exemplar] - \
+                            (best[1] if best[0] != exemplar else second_best[1]))\
+                            for exemplar in sim[point].keys()}
+
+        avail = {}
+        for exemplar in ordered_repos:
+            positive_resps = sum([max(0, resp[otherpoint][exemplar]) for otherpoint in r2r[exemplar].keys()])
+            avail[exemplar] = {point: \
+                                (oldavail[exemplar][point]*(1-damp) if oldavail != None else 0) + \
+                                (damp if oldavail != None else 1) * \
+                (min(0, resp[exemplar][exemplar] + positive_resps - max(0, resp[point][exemplar]) - max(0, resp[exemplar][exemplar])) \
+                    if point != exemplar else \
+                (positive_resps - max(0, resp[exemplar][exemplar]))) \
+                for point in r2r[exemplar].keys()}
+
+        oldresp, oldavail, damp = resp, avail, damp * damping
+
+    return resp, avail
+
+def gen_exemplars(resp, avail):
+    exemplars = {point: max(resp[point].keys(), key=lambda exemplar: resp[point][exemplar] + avail[exemplar][point]) \
+                for point in resp.keys()}
+    children = {}
+    for (point, exemplar) in exemplars.items():
+        if exemplar not in children:
+            children[exemplar] = []
+        children[exemplar].append(point)
+
+    return exemplars, children
+
+def collapseTreeNode(node):
+    if "children" in node:
+        for child in node["children"]:
+            collapseTreeNode(child)
+        if len(node["children"]) == 1:
+            if node["name"] != node["children"][0]["name"]:
+                raise ValueError("Expected " + node["name"] + " to equal " + node["children"][0]["name"])
+            elif "children" not in node["children"][0]:
+                del node["children"]
+            else:
+                node["children"] = node["children"][0]["children"]
+
+def recluster(repos, prev_r2r, prev_ch, num_iters, damping=0.95):
+    prev_exemplars = [x for x in prev_ch if x in prev_ch[x]]
+    next_r2r = {r1: {r2: prev_r2r[r1][r2] for r2 in prev_r2r[r1].keys() if r2 in prev_exemplars} for r1 in prev_r2r.keys() if r1 in prev_exemplars}
+    resp, avail = calc_similarities(next_r2r, repos, 0, num_iters, damping)
+    next_ex, next_ch = gen_exemplars(resp, avail)
+    return next_r2r, next_ch
+
+def eco_r2r(r2r, gitrank, ch, r1, r2):
+    return sum([gitrank[ch2] * r2r[ch1][ch2] for ch1 in ch[r1] if ch1 in r2r for ch2 in r2r[ch1].keys() if ch2 in ch[r2]]) \
+            / sum([gitrank[ch2] for ch2 in ch[r2]])
+
+def recluster_better(repos, gitrank, prev_r2r, prev_ch, num_iters, damping=0.95):
+    # Not better yet
+    prev_exemplars = [x for x in prev_ch if x in prev_ch[x]]
+    next_r2r = {r1: {r2: eco_r2r(prev_r2r, gitrank, prev_ch, r1, r2) for r2 in prev_r2r[r1].keys() if r2 in prev_exemplars} for r1 in prev_r2r.keys() if r1 in prev_exemplars}
+    resp, avail = calc_similarities(next_r2r, repos, 0, num_iters, damping)
+    next_ex, next_ch = gen_exemplars(resp, avail)
+    return next_r2r, next_ch
+
+def main():
+    data_path = "./downloaded_data"
+    repos, users = load_repos(data_path), load_users(data_path)
+    links = calc_graph(repos, users)
+    r2r, linkedrepos = repo_to_repo_links(links)
+    resp, avail = calc_similarities(r2r, repos, 0, 20)
+    ex, ch = gen_exemplars(resp, avail)
+
+    r2r_2, ch2 = recluster(repos, r2r, ch, 30, 0.97)
+    r2r_3, ch3 = recluster(repos, r2r_2, ch2, 50, 0.99)
+    r2r_4, ch4 = recluster(repos, r2r_3, ch3, 100, 0.99)
+
+    d3_gitmap = {"name": "github", "children": [ \
+                    {"name": root, "children": [ \
+                        {"name": greatgrandpa, "children": [ \
+                            {"name": grandpa, "children": [ \
+                                {"name": dad, "children": \
+                                    [{"name": child} \
+                                    for child in sorted(ch[dad])]} \
+                                for dad in sorted(ch2[grandpa])]} \
+                            for grandpa in sorted(ch3[greatgrandpa])]} \
+                        for greatgrandpa in sorted(ch4[root])]} \
+                    for root in sorted(ch4.keys())]}
+    collapseTreeNode(d3_gitmap)
+    full_gitmap = {"tree": d3_gitmap, "links": [(r1, r2) for (r1, r2, r3, r4) in linkedrepos]}
+    with open("gitmap.json", "w") as f:
+        f.write(json.dumps(full_gitmap)) #, indent=2))
+
+if __name__ == "__main__":
+    main()
